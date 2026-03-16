76bc388b054719e83ffa4f37ee5cddbc56b31beb
Camilo Sebastian <106637877+gabrielcosta-js@users.noreply.github.com>
2026-03-16 10:32:09 -0300

    enviando atividades de js

---
+++ 03-studies-senai/frontend-development/javaScript/exercise_list1/script.js
@@ -0,0 +1,20 @@
+function questao1(){
+    const nome = "Gabriel Costa de Jesus";
+    console.log(`Olá, ${nome} Seja bem-vindo(a) ao curso de JavaScript!`);
+}
+function questao2(){
+    const idadeNascimento = 2005;
+    const anoAtual = 2026;
+
+    idade = anoAtual - idadeNascimento;
+
+    console.log(`Sua idade atual é: ${idade} anos`);
+}
+
+function questao3(){
+    const cidade = prompt("Digite a sua cidade");
+    const estado = prompt("Digite seu estado");
+    const pais = prompt("DIgite seu páis");
+    
+    alert(`Sua cidade é: ${cidade}, seu estado é: ${estado}, seu país é ${pais}`);
+}
