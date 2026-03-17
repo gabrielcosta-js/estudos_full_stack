cb87e56a3a3de85fc2d98fbea60fecb3e7b414fa
Gabriel Costa de Jesus <106637877+gabrielcosta-js@users.noreply.github.com>
2026-03-17 11:21:35 -0300

    enviando atividade de js senai

--- 03-studies-senai/frontend-development/javaScript/exercise_list1/script.js
+++ 03-studies-senai/frontend-development/javaScript/exercise_list1/script.js
@@ -10,11 +10,55 @@ function questao2(){
 
     console.log(`Sua idade atual é: ${idade} anos`);
 }
-
 function questao3(){
     const cidade = prompt("Digite a sua cidade");
     const estado = prompt("Digite seu estado");
     const pais = prompt("DIgite seu páis");
     
     alert(`Sua cidade é: ${cidade}, seu estado é: ${estado}, seu país é ${pais}`);
+}
+function questao4(){
+    let temCarteira = true;
+    console.log(typeof(temCarteira));
+}
+function questao5(){
+    let saldo = 0;
+    console.log(`Saldo Atual: ${saldo}`);
+    console.log(`Depósito de 200`);
+    saldo += 200;
+    console.log(`Saldo Atual: ${saldo}`);
+    console.log(`Saque de 50`);
+    saldo -= 50;
+    console.log(`Saldo Atual: ${saldo}`);
+}
+function questao6(){
+    let matematica = 9;
+    let portugues = 7;
+    let ciencias = 8;
+
+    media = (matematica+portugues+ciencias)/3; 
+
+    console.log("Média", media);
+}
+function questao7(){
+    let salario = 3000;
+    salario = (salario * 0.1) + salario;
+
+    console.log("Parabéns seu salário de 3000 recebeu um aumento de 10%");
+    console.log(`Seu novo salário é: ${salario}`);
+
+}
+
+let clique = 0;
+function questao8(){
+
+    
+    clique++;
+
+    if(clique == 3) {
+        console.log(`Fim da Simulação: ${clique}`);
+
+    }
+   
+
 }
