5e0a95e1639198189f65dd0a6e0e5c8f7eda1317
Gabriel Costa JS <gabrielcosta-20@hotmail.com>
2026-05-19 21:47:40 -0300

    Enviando atividade de TypeScript com Nest

---
+++ 03-studies-senai/backend-development/nest/exercise01-CRUD/primeira-api/src/modules/produto/produto.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProdutoService } from './produto.service';
+import {Produto} from './interfaces/produto.interface';
+
+@Controller('produto')
+export class ProdutoController {
+    constructor(private produtoService: ProdutoService) {}
+
+    @Get()
+    encontrarTodosOsProdutos(): Array<Produto> {
+         return this.produtoService.encontrarTodosOsProdutos();
+    }
+
+    @Get('/:id')
+    encontrarUmProduto(@Param('id') id: number): Produto {
+        return this.produtoService.encontrarUmProduto(id);
+    } 
+
+    @Post()
+    criarProduto(@Body() data: Produto) : String {
+        return this.produtoService.criarProduto(data);
+    }
+
+    @Put('/:id')
+    atualizarProduto(@Param('id') id:number, @Body() data: Produto) {
+        this.produtoService.atualizarProduto(id, data);
+    }
+}
---
+++ 03-studies-senai/backend-development/nest/exercise01-CRUD/primeira-api/src/modules/produto/produto.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProdutoController } from './produto.controller';
+import { ProdutoService } from './produto.service';
+import { ProdutosRepository } from './produto.repository';
+
+@Module({
+  controllers: [ProdutoController],
+  providers: [ProdutoService]
+})
+export class ProdutoModule {}
